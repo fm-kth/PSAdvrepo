fbd683cc1db145ad46b711dfe4d960b92adc87da
Fredrik Magnusson <fm@kth.se>
2025-02-18 12:45:53 +0100

    funtions

--- MyFunctions.ps1
+++ MyFunctions.ps1
@@ -1,12 +1,38 @@
 # Labb 02 functions
+$MyUserListFile = "C:\Users\fm\PSAdvrepo\Labfiles\MyLabFile.csv"
+Get-Content -Path $MyUserListFile | ConvertFrom-Csv
+
 
 function GetUserData {
-$MyUserListFile = "$PSScriptRoot\MyLabFile.csv"
+$MyUserListFile = "C:\Users\fm\PSAdvrepo\Labfiles\MyLabFile.csv"
 $MyUserList = Get-Content -Path $MyUserListFile | ConvertFrom-Csv
 $MyUserList
 }
 
+#added snippet cmdlet ctrl+shift+p
 function Get-CourseUser {
-
-
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]$Name,
+        
+        [Parameter()]
+        [int]$OlderThan
+        #$OlderThan =  '65'
+    )
+    
+        $Result = GetUserData
+ 
+        
+        if (-not [string]::IsNullOrEmpty($Name)) {
+            $Result = $Result | Where-Object -Property Name -Like "*$Name*"
+        }
+        
+        if ($OlderThan) {
+            $Result = $Result | Where-Object -Property Age -ge $OlderThan
+       
+    
+        $Result
+        
+    }
 }
